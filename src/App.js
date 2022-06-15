f649305ad889dc2a6619672be681845c542bbfa7
VovaHash <vovahash01@gmail.com>
2022-06-15 22:30:19 +0300

    props added and assigned from external data file, done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Header from './components/Header'
 import Card from './components/Card'
+import data from './data'
 
 function App() {
+
+  const datasource = data.map((item)=>{
+    return(
+      <Card 
+        key={item.id}
+        item = {item}
+       />
+    )
+  })
+
   return (
    <>
       <Header/>
-      <Card/>
+      {datasource}
    </>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,21 @@
-export default function Card() {
+export default function Card(props) {
+  console.log(props)
   return (
     <div className="card">
       <img
         className="card--img"
-        src="https://source.unsplash.com/WLxQvbMyfas"
+        src={props.item.imageUrl}
       />
       <div className="card--content">
         <div className="card--content_subheader">
           <img src="./images/arrow.svg" />
-          <span className="card--content_location">JAPAN</span>
-          <span className="card--content_link">View on Google Maps</span>
+          <span className="card--content_location">{props.item.location}</span>
+          <a className="card--content_link" href={props.item.googleMapsUrl} target="_blank">View on Google Maps</a>
         </div>
-        <h2 className="header-content_header">Mount Fuji</h2>
-        <h4 className="card--content_dates">12 Jan, 2021 - 24 Jan, 2021</h4>
+        <h2 className="header-content_header">{props.item.title}</h2>
+        <h4 className="card--content_dates">{props.item.startDate} - {props.item.endDate}</h4>
         <p className="card--content_desc">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
+          {props.item.description}
         </p>
       </div>
     </div>
