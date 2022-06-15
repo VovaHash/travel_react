48cbe212ce3629c19c430924bbd13237f0f28b91
VovaHash <vovahash01@gmail.com>
2022-06-15 21:43:28 +0300

    markup and design done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,24 @@
 export default function Card() {
-    return(
-        <div className="card">
-            <img className="card--img" src="https://source.unsplash.com/WLxQvbMyfas"/>
+  return (
+    <div className="card">
+      <img
+        className="card--img"
+        src="https://source.unsplash.com/WLxQvbMyfas"
+      />
+      <div className="card--content">
+        <div className="card--content_subheader">
+          <img src="./images/arrow.svg" />
+          <span className="card--content_location">JAPAN</span>
+          <span className="card--content_link">View on Google Maps</span>
         </div>
-    )
-
+        <h2 className="header-content_header">Mount Fuji</h2>
+        <h4 className="card--content_dates">12 Jan, 2021 - 24 Jan, 2021</h4>
+        <p className="card--content_desc">
+          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
+          (12,380 feet). Mount Fuji is the single most popular tourist site in
+          Japan, for both Japanese and foreign tourists.
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header className="header">
-            <img className="header--icon" src="./images/fill.svg"/>
+            <img className="header--icon" src="./images/globe.svg"/>
             <h1 className="header--title">my travel journal.</h1>
         </header>
     )
