bc282c407c3551fa6c069fb22b164c869e84fa11
VovaHash <vovahash01@gmail.com>
2022-06-15 20:19:13 +0300

    header done, now card

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+export default function Card() {
+    return(
+        <div className="card">
+            <img className="card--img" src="https://source.unsplash.com/WLxQvbMyfas"/>
+        </div>
+    )
+
+}
